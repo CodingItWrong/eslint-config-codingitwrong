e796d6eda5c23baafbb725e9f7d30f690c91b803
Josh Justice <jjustice@bignerdranch.com>
2018-05-27 16:53:20 -0400

    Create initial version

---
+++ README.md
@@ -0,0 +1,30 @@
+# eslint-config-codingitwrong
+
+The goal of this ESLint config is to enforce safety and a consistent style, while not getting in the way of refactoring. In particular:
+
+- Unused variables (and thus imports) are allowed, so that if you are temporarily not using a variable you don't have to comment it out elsewhere.
+- Arrow parens and body style are not restricted, so you can add or remove parameters, or switch it between a block and expression without needing to change the style.
+- Quote style is not enforced so you won't need to change backticks to another type of quote if you temporarily remove all template expressions.
+- Constant conditions are allowed so you can do `if (true)` or `if (false)` to temporarily get a branch to always pass.
+- `console.log()`, `alert()`, and `debugger` are allowed. How else are you going to get anything done??
+- Unreachable code is allowed so you can put an early `return` in a block for experimentation.
+
+However, curlies are required; it's just too risky to allow leaving them out.
+
+## Usage
+
+Install the package:
+
+`npm install --save-dev eslint-config-codingitwrong`
+
+Then set it in your ESLint config file:
+
+```javascript
+module.exports = {
+  "extends": "codingitwrong",
+};
+```
+
+## License
+
+Apache
---
+++ index.js
@@ -0,0 +1,62 @@
+module.exports = {
+  'rules': {
+    // safety
+    'curly': ['error', 'all'],
+    'eqeqeq': ['error', 'always'],
+    'no-eval': ['error'],
+    'no-implied-eval': ['error'],
+    'no-return-await': ['error'],
+    'no-template-curly-in-string': ['error'],
+    'no-var': ['error'],
+    'no-with': ['error'],
+    'require-await': ['error'],
+    'semi': ['error', 'always'],
+
+    // diffs
+    'comma-dangle': [
+      'error',
+      {
+        'arrays': 'only-multiline',
+        'exports': 'only-multiline',
+        'functions': 'only-multiline',
+        'imports': 'only-multiline',
+        'objects': 'only-multiline',
+      }
+    ],
+
+    // enabling refactoring
+    'no-alert': ['off'],
+    'no-console': ['off'],
+    'no-debugger': ['off'],
+    'no-constant-condition': ['off'],
+    'no-unreachable': ['off'],
+
+    // simplicity
+    'no-duplicate-imports': ['error'],
+
+    // style
+    'indent': ['error', 2],
+    'max-len': ['error', { 'code': 80 }],
+    'array-bracket-spacing': ['error', 'never'],
+    'arrow-spacing': ['error'],
+    'block-spacing': ['error', 'always'],
+    'brace-style': ['error', '1tbs'],
+    'camelcase': ['error'],
+    'comma-spacing': ['error'],
+    'comma-style': ['error', 'last'],
+    'eol-last': ['error', 'always'],
+    'func-call-spacing': ['error', 'never'],
+    'key-spacing': ['error'],
+    'keyword-spacing': ['error'],
+    'linebreak-style': ['error', 'unix'],
+    'no-multiple-empty-lines': ['error', { 'max': 1 }],
+    'no-trailing-spaces': ['error'],
+    'no-whitespace-before-property': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    'padded-blocks': ['error', 'never'],
+    'space-before-blocks': ['error', 'always'],
+    'space-before-function-paren': ['error', 'never'],
+    'space-in-parens': ['error', 'never'],
+    'template-curly-spacing': ['error', 'never'],
+  }
+};
